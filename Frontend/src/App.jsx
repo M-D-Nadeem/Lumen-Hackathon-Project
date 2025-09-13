c5d64fb5a920abb1cec211bf62e7895d9403882e
Jagruthi Reddy <jagruthireddynagireddy@gmail.com>
2025-09-13 14:03:34 +0530

    Added subscription portal jsx and css files under components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import AdminDashboard from "./Pages/AdminDashboard";
 import Plans from "./Pages/Plans";
+import SubscriptionPortal from "./Components/SubscriptionPortal";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<h2>Welcome Home</h2>} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/plans" element={<Plans />} />
+        <Route path="/subscription" element={<SubscriptionPortal />} />
       </Routes>
     </>
   );
