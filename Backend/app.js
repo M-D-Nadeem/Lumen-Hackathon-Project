99c671881a11818029b70d29d56be634a2d1990c
shaikkarimulla <shaikkarimulla66656@gmail.com>
2025-09-13 12:14:51 +0530

    Added Subscription Controllers nad Routes

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,13 @@
 import express from "express"
 import cors from "cors"
+import subscriptionRoutes from "./routes/subscriptionRoutes.js";
+
 const app = express();
 
 app.use(cors());                 
 app.use(express.json());        
 
+app.use("/api/subscriptions", subscriptionRoutes);
 
 app.get('/', (req, res) => {
   res.send('Backend is running');
